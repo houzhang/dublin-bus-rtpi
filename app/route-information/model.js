95dcbde38b04a5fc3498c330611fb1b1d46e6ade
Houzhang Liang <houzhang@gmail.com>
2018-09-18 21:49:43 +0100

    added route-information

---
+++ app/route-information/model.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  operator: DS.attr('string'),
+  origin: DS.attr('string'),
+  destination: DS.attr('string')
+});
--- app/route-list-information/serializer.js
+++ app/route-list-information/serializer.js
@@ -3,20 +3,22 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
   normalizeArrayResponse(store, primaryModelClass, payload, id) {
-
-    payload.results.forEach(element => {
-      element.id = element.operator + "_" + element.route
-    });
-
-    payload.routeListInformation = payload.results
-
-    delete payload.errorcode
-    delete payload.errormessage
-    delete payload.numberofresults
-    delete payload.timestamp
-    delete payload.results
-    
+    if (payload.errorcode == 0) {
+      payload.results.forEach(element => {
+        element.id = element.operator + "_" + element.route
+      });
+  
+      payload.routeListInformation = payload.results
+  
+      delete payload.errorcode
+      delete payload.errormessage
+      delete payload.numberofresults
+      delete payload.timestamp
+      delete payload.results            
+    } else {
+      throw new Error(payload.errormessage)
+    }    
     return this._super(...arguments);
-  },
+  }
 
 });
